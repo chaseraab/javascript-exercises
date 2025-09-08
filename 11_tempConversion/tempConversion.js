935ebe58087f831cdf1145ef028585234a303b02
craab220@gmail.com <99097952+chaseraab@users.noreply.github.com>
2025-09-07 20:34:33 -0500

    Adding assignment 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,28 @@
-const convertToCelsius = function() {
+function checkMantissa(flt) {
+  let str = flt.toString()
+  if (!(str.includes("."))) {
+    return 0
+  } else {
+    return str.split(".")[1].length
+  }
+}
+const convertToCelsius = function(temp) {
+  let cels = (temp - 32) * (5/9)
+  console.log("from CheckMantissa " + checkMantissa(cels))
+    if (checkMantissa(cels) < 2) {
+    return cels
+  } else {
+    return Number(cels.toFixed(1))
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahre = (temp * (9/5) + 32)
+  if (checkMantissa(fahre) < 2) {
+    return fahre
+  } else {
+    return Number(fahre.toFixed(1))
+  }
 };
 
 // Do not edit below this line
